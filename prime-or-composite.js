f471880ed8b53bd2b884b84140cc43595858b79a
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-08-28 20:36:31 -0600

    Create a program in js that determines if number is prime
    This program is a javascript program that will list numbers from 2 to a number put into the function telling whether they are prime numbers or composite numbers.

---
+++ prime-or-composite.js
@@ -0,0 +1,24 @@
+function primeOrComposite(num) {
+    let i = 2;
+    while (i < num) {
+	let prime = true;
+	let k = 2;
+	for(k; k < i; k++) {
+	    if(i % k === 0 && k !== i) {
+		prime = false;
+	    }
+	    if (!prime) {
+		break;
+	    }
+	}
+	if (prime) {
+	    console.log(i + " is a prime number.");
+	} else {
+	    console.log(i + " is a composite number.");
+	}
+	i++
+    }
+}
+
+primeOrComposite(100);
+
